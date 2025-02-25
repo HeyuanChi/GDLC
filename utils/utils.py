0b90c8e6483cd53fc8589efad9055920f22c42f0
HeyuanChi <heyuan.chi.de@gmail.com>
2025-02-25 23:00:05 +0100

    feat: predict

--- utils/utils.py
+++ utils/utils.py
@@ -9,3 +9,33 @@ def get_device():
     device = torch.device("cuda" if use_cuda else "mps" if use_mps else "cpu")
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda or use_mps else {}
     return device, kwargs
+
+def predict(model, data_loader, flows=False):
+    device, kwargs = get_device()
+
+    model.to(device)
+    model.eval()
+
+    all_inputs = []
+    all_outputs = []
+    all_targets = []
+
+    with torch.no_grad():
+        for inputs, targets in data_loader:
+            inputs = inputs.to(device).float()
+            targets = targets.to(device).float()
+            if flows:
+                outputs, nll = model(inputs)
+            else:
+                outputs = model(inputs)
+
+            all_inputs.append(inputs)
+            all_outputs.append(outputs)
+            all_targets.append(targets)
+
+    all_inputs = torch.cat(all_inputs, dim=0)
+    all_outputs = torch.cat(all_outputs, dim=0)
+    all_targets = torch.cat(all_targets, dim=0)
+
+    return all_inputs, all_outputs, all_targets
+
