2853733ecb5017a8e3b400f779b3202348be3c49
HeyuanChi <heyuan.chi.de@gmail.com>
2025-02-26 00:52:27 +0800

    feat: FWI unet

--- model/unet.py
+++ model/unet.py
@@ -50,12 +50,14 @@ class Up(nn.Module):
         if self.resize == 'padding':
             diffY = x2.size(2) - x1.size(2)
             diffX = x2.size(3) - x1.size(3)
-            x1 = F.pad(x1, [(diffX + 1) // 2, diffX // 2, (diffY + 1) // 2, diffY // 2])
+            if diffY > 0 or diffX > 0:
+                x1 = F.pad(x1, [(diffX + 1) // 2, diffX // 2, (diffY + 1) // 2, diffY // 2])
         
         elif self.resize == 'cropping':
             diffY = x2.size(2) - x1.size(2)
             diffX = x2.size(3) - x1.size(3)
-            x2 = x2[:, :, (diffY + 1) // 2 : - diffY // 2, (diffX + 1) // 2 : - diffX // 2]
+            if diffY > 0 or diffX > 0:
+                x2 = x2[:, :, (diffY + 1) // 2 : - diffY // 2, (diffX + 1) // 2 : - diffX // 2]
         
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
@@ -101,4 +103,38 @@ class FillUNet(nn.Module):
         x = self.up4(x, x1)    # ~ [B,   64, 230, 230]
 
         logits = self.outc(x)  # ~ [B,    1, 230, 230]
+        return logits
+
+
+class FWIUNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1):
+        super().__init__()
+
+        # Encoder
+        self.inc = DoubleConv(in_channels, 16)
+        self.down1 = Down(16,   32, dropout=0.1)
+        self.down2 = Down(32,   64, dropout=0.2)
+        self.down3 = Down(64,  128, dropout=0.2)
+        self.down4 = Down(128, 256, dropout=0.3)
+
+        # Decoder
+        self.up1 = Up(256 + 128, 128, dropout=0.2, resize='cropping')
+        self.up2 = Up(128 +  64,  64, dropout=0.2, resize='cropping')
+        self.up3 = Up(64  +  32,  32, dropout=0.1, resize='cropping')
+        self.up4 = Up(32  +  16,  16, dropout=0.1, resize='cropping')
+        self.outc = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.inc(x)       # ~ [B,  16, 230, 230]
+        x2 = self.down1(x1)    # ~ [B,  32, 115, 115]
+        x3 = self.down2(x2)    # ~ [B,  64,  57,  57]
+        x4 = self.down3(x3)    # ~ [B, 128,  28,  28]
+        x5 = self.down4(x4)    # ~ [B, 256,  14,  14]
+
+        x = self.up1(x5, x4)   # ~ [B, 128,  28,  28]
+        x = self.up2(x, x3)    # ~ [B,  64,  56,  56]
+        x = self.up3(x, x2)    # ~ [B,  32, 112, 112]
+        x = self.up4(x, x1)    # ~ [B,  16, 224, 224]
+
+        logits = self.outc(x)  # ~ [B,   1, 224, 224]
         return logits
